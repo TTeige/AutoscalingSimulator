5c141618dd560a000782c42def8893387142f92c
TTeige <tim.teige91@gmail.com>
2017-04-06 08:15:10 +0200

    Completed the test structs for the test xml which should be marshaled from the http POST request.

--- SimulatorServer.go
+++ SimulatorServer.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"io"
-	"strings"
 	"net/http"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
 )
-
 
 var mux map[string]func(w http.ResponseWriter, r *http.Request)
 
 type handler struct{}
+
+type Query struct {
+
+}
+
+type QueueData struct {
+	XMLName  xml.Name `xml:"Data"`
+	MetaJobs []MetaJob `xml:"MetaJob"`
+}
+
+type MetaJob struct {
+	XMLName     xml.Name `xml:"MetaJob"`
+	Id          string `xml:"id,attr"`
+	Platform    string `xml:"platform,attr"`
+	Runtime     int `xml:"Runtime"`
+	Scalability float64 `xml:"Scalability"`
+	Resources   []Node `xml:"Resources>Node"`
+}
+
+type Node struct {
+	XMLName xml.Name `xml:"Node"`
+	Id      string `xml:"id,attr"`
+	Memory  int `xml:"Memory"`
+}
 
 func (*handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if h, ok := mux[r.URL.String()]; ok {
@@ -20,16 +45,29 @@ func (*handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func requestHandleRoot(w http.ResponseWriter, r *http.Request) {
-	if strings.Compare(r.Method, "POST") {
-		if strings.Compare(r.Header.Get("content-type"), "xml") {
+	if r.Method == "POST" {
+		if r.Header.Get("Content-Type") == "application/xml" {
+			//decoded := xml.NewDecoder(r.Body)
 
+			data := QueueData{}
+
+			//err := decoded.Decode(&data)
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Printf("Error while parsing the body to bytes: %s",err)
+			}
+			err = xml.Unmarshal(body, &data)
+			if err != nil {
+				io.WriteString(w, "Error reading the xml data for the Data element.\n")
+				fmt.Printf("Error reading xml data: %s\n", err)
+			}
 		}
 	}
 	io.WriteString(w, "Connected\n")
 }
 
 func requestHandleQueryResource(w http.ResponseWriter, r *http.Request) {
-	io.WriteString()
+	io.WriteString(w, "Resource goes here\n")
 }
 
 func main() {
