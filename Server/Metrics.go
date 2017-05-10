dd585f5b5646802ddb3186c691495a1971c15b1e
TTeige <tim.teige91@gmail.com>
2017-05-10 12:42:25 +0200

    Modified the currently unused Metrics to use the built in struct from psutil. This will most likely never be used, but exists as a placeholder for now. Will update to the spark web-gui interface at a later time

--- Server/Metrics.go
+++ Server/Metrics.go
@@ -1,16 +1,14 @@
 package server
 
-type NodeTimeMetric struct {
-	CPU       string
-	User      float64
-	System    float64
-	Idle      float64
-	Nice      float64
-	Iowait    float64
-	Irq       float64
-	Softirq   float64
-	Steal     float64
-	Guest     float64
-	GuestNice float64
-	Stolen    float64
+import (
+	"github.com/shirou/gopsutil/cpu"
+)
+
+func randPopulate() cpu.TimesStat {
+
+	return cpu.TimesStat{
+		CPU:"Simulator Node, randomly populated",
+		User:16.0,
+
+	}
 }
